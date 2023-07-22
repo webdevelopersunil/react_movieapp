707225ddbf033a43d62ab188fcab968eae833ca8
webdevelopersunil <he.kingsmen@gmail.com>
2023-07-22 23:14:05 +0530

    Column for movie app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import MovieCard from "./MovieCard.js";
+import css from "./index.css";
+
+
+function App() {
+  return (
+    <>
+      <h1>Movie App</h1>
+      <MovieCard />
+       
+    </>
+  );
+}
+
+export default App;
