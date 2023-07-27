d2dec785db866121cc1196e64081f0849e7a3122
webdevelopersunil <he.kingsmen@gmail.com>
2023-07-27 19:19:50 +0530

    props using the function based component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 // import MovieList from "./MovieList.js";
 import "./index.css";
 import Student from "./Student";
+import List from "./Components/List";
 
 function App() {
   return (
     <>
       <h1>Movie App</h1>
       {/* <MovieList /> */}
-      { <Student name="Sunil" sname="Thakur" /> }
+      { <Student name="Sunil" sname="Thakur" nilam="nilam" /> }
+
+      {/* {< List />} */}
     </>
   );
 }
--- src/Student.js
+++ src/Student.js
@@ -1,19 +1,35 @@
 import {Component} from "react";
 
-class Student extends Component{
 
-    render(){
+// using  class based component with props
+// class Student extends Component{
 
-        // const { title, plot, price, rating, stars, fav, cart } = this.state;
-        console.log(this.props);
-        const {name, sname} = this.props;
-        return (
-            <>
+//     render(){
+
+//         // const { title, plot, price, rating, stars, fav, cart } = this.state;
+//         console.log(this.props);
+//         const {name, sname} = this.props;
+//         return (
+//             <>
+//             <h1>Hello, {name}</h1>
+//             <p>You are a {sname}</p>
+//             </>
+//         )
+//     }
+// }
+
+
+// using the function based component with props
+
+function Student(props){
+    const {name, sname} = this.props;
+    return(
+        <>
             <h1>Hello, {name}</h1>
             <p>You are a {sname}</p>
-            </>
-        )
-    }
+
+        </>
+    )
 }
 
 export default Student;
