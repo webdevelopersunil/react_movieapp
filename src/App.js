eac8f11e24fa836db52b9938fae0bde8ff0ad39c
webdevelopersunil <he.kingsmen@gmail.com>
2023-07-31 20:03:47 +0530

    Navbar Added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-// import MovieList from "./MovieList.js";
+import MovieList from "./MovieList.js";
 import "./index.css";
 import Student from "./Student";
 import List from "./Components/List";
+import Navbar from "./Navbar.js";
 
 function App() {
   return (
     <>
       <h1>Movie App</h1>
-      {/* <MovieList /> */}
-      { <Student name="Sunil" sname="Thakur" nilam="nilam" /> }
+      <Navbar />
+      <MovieList />
+      {/* { <Student name="Sunil" sname="Thakur" nilam="nilam" /> } */}
 
       {/* {< List />} */}
     </>
