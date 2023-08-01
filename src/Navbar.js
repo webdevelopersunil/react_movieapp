47e5f433ec129cc2ec29638a8c9444f4cab235db
webdevelopersunil <he.kingsmen@gmail.com>
2023-08-01 22:48:50 +0530

    Dynamic CSS

--- src/Navbar.js
+++ src/Navbar.js
@@ -19,6 +19,7 @@ const Title = styled.div`
     font-family: 'Times New Roman', Times, serif;
     text-transform: uppercase;
     margin-left: 20px ;
+    &:hover{color: blue}
 `;
 
 
@@ -33,13 +34,14 @@ const CartIconContainer = styled.div`
 `;
 
 const CartCount = styled.div`
-    background: yellow;
+    background: ${(props=>props.color)};
     border-radius: 50%;
     padding: 4px 8px;
     position: absolute;
     right: 10px;
     top: -5px;
     font-size: 12px;
+    visibility : ${(props) => props.show ? "visible" : "hidden"};
 `;
 
 
@@ -59,7 +61,7 @@ class Navbar extends React.Component {
             <CartImg alt="Cart-Icon" 
                  src = "https://cdn-icons-png.flaticon.com/128/891/891462.png"    
             />
-            <CartCount >5</CartCount>
+            <CartCount color="yellow" show={true} >5</CartCount>
         </CartIconContainer>
       </Nav>
 
