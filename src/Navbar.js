1b4e633a3841f0adef67938e2ca50393cda21443
webdevelopersunil <he.kingsmen@gmail.com>
2023-08-01 22:11:06 +0530

    styling css

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,17 @@
 import React from "react";
+import styled from "styled-components";
 
 class Navbar extends  React.Component{
     render(){
         return (
             <>
-            <div>
-                <div>Title</div>
+            <div className="nav" >
+                <div className="title" >
+                    Movie-App
+                </div>
                 <div>
-                    <img alt="Cart Icon" />
-                    <span>0</span>
+                    <img alt="Cart Icon" alt="Cart Icon" />
+                    <span>3</span>
                 </div>
             </div>
             </>
